e0166500ab2e7274bef857ac754920267ecf6350
Tim Dosé <tim.dose@zocdoc.com>
2016-10-05 22:31:42 -0400

    auto select carrier if you try to delete it

--- tags.js
+++ tags.js
@@ -1,10 +1,93 @@
+const KEY_LEFT_ARROW = 37;
+const KEY_RIGHT_ARROW = 39;
+const KEY_UP_ARROW = 38;
+const KEY_DOWN_ARROW = 40;
+const KEY_RETURN = 13;
+const KEY_TAB = 9;
+const KEY_DELETE = 8;
+
 require([
     '/jquery.js',
     '/lodash.js',
     '/mustache.js',
     'text!templates/carrier-list.mustache',
+    'text!templates/plan-list.mustache',
     'text!insurance_data.json'
-], function (jquery, _, Mustache, carrierTemplate, insuranceData ) {
+], function (jquery, _, Mustache, carrierTemplate, planTemplate, insuranceData ) {
+    ///////////////////////////////////////////
+    // global functions
+    ///////////////////////////////////////////
+    var currentState = 'carriers';
+    
+    setCarrier = function( carrier ) {
+        var carrierName = $(carrier).find('.item').text();
+        var truncated = carrierName;
+        console.log(truncated);
+        if ( truncated.length > 20 ) {
+            truncated = carrierName.substr(0,5) + '...' + carrierName.substr(carrierName.length-5)
+        }
+        truncated += ' - ';
+        $('#search').val(truncated);
+        $('#carrier-truncated').val(truncated);
+        $('#carrier').val(carrierName);
+        $('#search').focus();
+
+        currentState = 'plans';
+
+        renderPlans();
+    }
+
+    highlightListItem = function(itemNumber) {
+        if ( itemNumber === undefined ) itemNumber = 0;
+        $('#main-list-container li').eq(itemNumber).addClass('highlight');
+    }
+
+    backToCarrier = function() {
+        var carrier =  $('#carrier').val()
+        $('#carrier').val('');
+        $('#carrier-truncated').val('');
+        $('#search').val(carrier)                
+            .get(0).setSelectionRange(0,carrier.length);
+
+        currentState = 'carriers';
+        renderCarriers(carriers);
+    }
+
+    renderPlans = function() {
+        var plans = [
+            { id: 1, carrier: 'Carrier', carrierDisplay: 'Carrier', plan: 'HMO', planDisplay: 'HMO', requests: 10 },
+            { id: 2, carrier: 'Carrier', carrierDisplay: 'Carrier', plan: 'PPO', planDisplay: 'PPO', requests: 10 },
+            { id: 3, carrier: 'Carrier', carrierDisplay: 'Carrier', plan: 'EPO', planDisplay: 'EPO', requests: 10 }
+        ]
+
+        $('#main-list-container')
+            .empty()
+            .append(Mustache.to_html(planTemplate,{plans:plans}))
+    }
+
+    renderCarriers = function (carriers) {
+        console.log(carriers);
+        // carriers.sort();
+
+        $('#main-list-container')
+            .empty()
+            .append(Mustache.to_html(carrierTemplate,{carriers:carriers}))
+
+        $('#main-list-container li')
+            .hover(function() {
+                $('#main-list-container li').removeClass('highlight');
+                $(this).addClass('highlight')
+            })
+            .click(function(){
+                setCarrier(this);
+            });
+    }
+
+
+
+    ///////////////////////////////////////////
+    // local functions
+    ///////////////////////////////////////////
     var plans = JSON.parse(insuranceData)
         .map(function (raw) {
             return {
@@ -23,19 +106,78 @@ require([
     });
 
 
-    var renderCarriers = function (carriers) {
-        console.log(carriers);
-        // carriers.sort();
+    
+    var showSelection = function() {
+        var truncated = carrierName = $(this).val();
+        if ( truncated.length > 20 ) {
+            truncated = carrierName.substr(0,5) + '...' + carrierName.substr(carrierName.length-5)
+        }
+        $('#search').val(truncated);
+    };
 
-        $('#main-list-container')
-            .empty()
-            .append(Mustache.to_html(carrierTemplate,{carriers:carriers}))
+    var logSelection = function (field) {
+        var selectedText = $(field).val().substring(field.selectionStart, field.selectionEnd)
+        console.log(field.selectionStart + '-' + field.selectionEnd + ': ' + selectedText)
     }
+
+
+    ///////////////////////////////////////////
+    // event stuff
+    ///////////////////////////////////////////
+    $('#carrier')
+        .change(showSelection)
+        .click(showSelection);
+
+    $('#search').keyup(function(e) {
+        // console.log(e);
+        if (e.keyCode === KEY_LEFT_ARROW || e.keyCode == KEY_RIGHT_ARROW || e.keyCode == KEY_DELETE ) {
+            logSelection(this);
+        }
+    });
+
+    $('#search').mouseup(function() {
+        if (this.selectionStart < $('#carrier-truncated').val().length) {
+            this.setSelectionRange(0,this.selectionEnd);
+        }
+    });
+
+    $('#search').keydown(function(e) {
+        // console.log(e);
+        if (e.keyCode == KEY_DOWN_ARROW ) {
+            var $selected = $('#main-list-container .highlight');
+            if ( $selected.next().length > 0 ) {
+                $selected.removeClass('highlight');
+                $selected.next().addClass('highlight');
+            }
+        }
+
+        if (e.keyCode == KEY_UP_ARROW ) { 
+            var $selected = $('#main-list-container .highlight');
+            if ( $selected.prev().length > 0 ) {
+                $selected.removeClass('highlight');
+                $selected.prev().addClass('highlight');
+            }
+        }
+
+        if (e.keyCode == KEY_TAB || e.keyCode == KEY_RETURN ) {
+            if (currentState == 'plans') return;
+            setCarrier($('#main-list-container .highlight'));
+        }
+
+        if (e.keyCode == KEY_DELETE ) {
+            if (this.selectionStart <= $('#carrier-truncated').val().length ) {
+                backToCarrier();
+                return false;
+            }
+        }
+    });
+
+    
+
+    ///////////////////////////////////////////
+    // initialize
+    ///////////////////////////////////////////
 
     renderCarriers(carriers);
-
-    window.setCarrier = function( carrier ) {
-        console.log( $(carrier).find('.item').text() );
-    }
-
+    window.highlightListItem();
 })
