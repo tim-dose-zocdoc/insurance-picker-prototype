d0cd1c96a4c6cea0564442da035a0d3b53e5d355
Tim Dosé <tim.dose@zocdoc.com>
2016-10-07 10:14:38 -0400

    removed old printable key stuff

--- tags2.js
+++ tags2.js
@@ -156,16 +156,6 @@ require([
     });
 
 
-    var isPrintableKey = function (keyCode) {
-        return
-            (keyCode > 47 && keyCode < 58)   || // number keys
-            keyCode == 32                    || // spacebar
-            (keyCode > 64 && keyCode < 91)   || // letter keys
-            (keyCode > 95 && keyCode < 112)  || // numpad keys
-            (keyCode > 185 && keyCode < 193) || // ;=,-./` (in order)
-            (keyCode > 218 && keyCode < 223);   // [\]' (in order)
-    }
-
     ///////////////////////////////////////////
     // event stuff
     ///////////////////////////////////////////
